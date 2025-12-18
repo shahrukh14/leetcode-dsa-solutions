a30122d7bf7a89e050727358fe1a39893954c4ec
Mohammed Shahrukh <105311061+shahrukh14@users.noreply.github.com>
2025-12-18 18:57:44 +0530

    Time: 3 ms (58.68%), Space: 65.3 MB (72.38%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+    let minPrice = prices[0];
     let maxProfit = 0;
 
-    for(let i = 0; i < prices.length; i++){
-        for(let j = i + 1; j < prices.length; j++){
-            let profit = prices[j] -  prices[i];
-            if(profit > maxProfit){
-                maxProfit = profit;
-            }
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
         }
     }
-
     return maxProfit;
 };
