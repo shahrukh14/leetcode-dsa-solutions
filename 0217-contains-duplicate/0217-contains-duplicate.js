68389660c9082c7e9a53adc0469325710de08af6
Mohammed Shahrukh <105311061+shahrukh14@users.noreply.github.com>
2026-03-06 18:48:10 +0530

    Time: 20 ms (42.95%), Space: 73.8 MB (71.51%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,9 +2,9 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-
 var containsDuplicate = function(nums) {
     const seen = new Set();
+
     for (let num of nums) {
         if (seen.has(num)) {
             return true;
