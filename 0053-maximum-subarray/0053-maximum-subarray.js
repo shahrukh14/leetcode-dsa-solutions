44483b397e218edb333e627e233f6dfba843f234
Mohammed Shahrukh <105311061+shahrukh14@users.noreply.github.com>
2026-03-15 18:59:41 +0530

    Time: 3 ms (44.13%), Space: 63.8 MB (65.69%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,12 +3,13 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let maxSum = nums[0];
     let currentSum = nums[0];
-    
+    let maxSum = nums[0];
+
     for (let i = 1; i < nums.length; i++) {
         currentSum = Math.max(nums[i], currentSum + nums[i]);
         maxSum = Math.max(maxSum, currentSum);
     }
+
     return maxSum;
 };
