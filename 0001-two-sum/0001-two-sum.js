a90c29098fe28965584a12e8c9f40b18d056ab3b
Mohammed Shahrukh <105311061+shahrukh14@users.noreply.github.com>
2026-03-24 21:57:01 +0530

    Time: 1 ms (87.04%), Space: 54.7 MB (60.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,15 +3,18 @@
  * @param {number} target
  * @return {number[]}
  */
-function twoSum(nums, target) {
-    const map = {}; 
+var twoSum = function(nums, target) {
+    const map = {}; // value -> index
+
     for (let i = 0; i < nums.length; i++) {
         const complement = target - nums[i];
 
+        // Check if complement exists
         if (map.hasOwnProperty(complement)) {
             return [map[complement], i];
         }
 
+        // Store current number with index
         map[nums[i]] = i;
     }
-}
+};
