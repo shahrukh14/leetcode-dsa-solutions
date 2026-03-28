0044da52859f7124d25804306523ed313eded789
Mohammed Shahrukh <105311061+shahrukh14@users.noreply.github.com>
2026-03-28 21:55:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -7,19 +7,14 @@ function maxProfit(prices) {
     let maxProfit = 0;
 
     for (let price of prices) {
-        // update minimum buying price
         if (price < minPrice) {
             minPrice = price;
         }
 
-        // calculate profit if sold today
         let profit = price - minPrice;
-
-        // update max profit
         if (profit > maxProfit) {
             maxProfit = profit;
         }
     }
-
     return maxProfit;
 }
