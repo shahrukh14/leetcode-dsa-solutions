75934ff62675c5488d8e3d150a9e10a54134f87b
Mohammed Shahrukh <105311061+shahrukh14@users.noreply.github.com>
2026-04-23 21:32:10 +0530

    Time: 30 ms (25.49%), Space: 77.1 MB (20.85%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,13 +4,11 @@
  */
 var containsDuplicate = function(nums) {
     const seen = {};
-
     for (let num of nums) {
         if (seen[num]) {
             return true;
         }
         seen[num] = true;
     }
-
     return false;
 };
